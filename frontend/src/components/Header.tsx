2b2d71c3bba56a5272169981d81c77d12c7bef93
Tiago061 <Thenriqueleite277@gmail.com>
2025-03-05 22:06:00 -0300

    feat: add header with react

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NavList from './NavList'
 import SearchBar from './SeachBar';
+import styles from './Header.module.css'
 
 
 function Header(){
     return(
         <div>
-            <header className="header">
-                <nav className="nav-header"> 
+            <header className={styles.header}>
+                <nav className={styles.nav_header}> 
                     <NavList />
                     <SearchBar />
                 </nav>
