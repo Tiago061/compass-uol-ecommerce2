569c6ec3f4f237dfc03da6d283ed85dd4c8a3351
Tiago061 <Thenriqueleite277@gmail.com>
2025-03-11 03:19:00 -0300

    feat: add all routes products

--- backend/src/routes/productsRouter.ts
+++ backend/src/routes/productsRouter.ts
@@ -3,9 +3,12 @@ import { ProductController } from "../controller/ProductController";
 
 const productsRouter = Router();
 
-const productController = new ProductController();
-
-productsRouter.get('/products', productController.getAll.bind(productController)) 
+const productController = new ProductController
+productsRouter.get("/products", productController.getAll);
+productsRouter.get("/products/:id", productController.getById);
+productsRouter.post("/products", productController.create);
+productsRouter.put("/products/:id", productController.update);
+productsRouter.delete("/products/:id", productController.delete);
 
 
 
