4f5c3eb017823310bff9c72de6b26df158f1cfe4
semnic <thenriqueleite277@gmail.com>
2025-03-12 21:11:47 -0300

    refactor: all code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,78 +1,13 @@
-import { Header } from './components/layout/Header';
-import { Hero } from './components/home/Hero';
-import { Brands } from './components/home/Brands';
-import { ProductSection } from './components/home/ProductSection';
-import { Footer } from './components/layout/Footer';
-import { Newsletter } from './components/home/Newsletter';
 
 
-const newArrivals = [
-  {
-    id: '1',
-    image: '/assets/img/Frame32.svg',
-    title: 'T-shirt with Tape Details',
-    price: 120
-  },
-  {
-    id: '2',
-    image: '/assets/img/Frame33.svg',
-    title: 'Skinny Fit Jeans',
-    price: 240,
-    originalPrice: 260
-  },
-  {
-    id: '3',
-    image: '/assets/img/Frame34.svg',
-    title: 'Checkred Shirt',
-    price: 180
-  },
-  {
-    id: '4',
-    image: '/assets/img/Frame38.svg',
-    title: 'Sleeve Striped T-shirt',
-    price: 130,
-    originalPrice: 160
-  }
-];
 
-const topSelling = [
-  {
-    id: '1',
-    image: '/assets/img/vertical.svg',
-    title: 'Vertical Striped Shirt',
-    price: 212,
-    originalPrice: 232
-  },
-  {
-    id: '2',
-    image: '/assets/img/courage.svg',
-    title: 'Courage Graphic T-shirt',
-    price: 145
-  },
-  {
-    id: '3',
-    image: '/assets/img/bermuda.svg',
-    title: 'Loose Fit Bermuda Shorts',
-    price: 80
-  },
-  {
-    id: '4',
-    image: '/assets/img/jeans.svg',
-    title: 'Faded Skinny Jeans',
-    price: 210
-  }
-];
+
 
 function App() {
   return (
-    <div className="min-h-screen m-0">
-      <Header />
-      <main>
-        <ProductSection title="new arrivals" products={newArrivals} />
-        <ProductSection title="top selling" products={topSelling} />
-      </main>
-      <Footer />
-    </div>
+    <>
+   
+    </>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,9 +12,10 @@ module.exports = {
       },
         colors: {
           'bg-primary': '#F2F0F1', 
-          'bg-cards': '##000000', 
+          'bg-cards': '#000000', 
           'bg-footer': '#F0F0F0',
           'color-font': '#00000099',
+          'color-font2': '##00000066',
         },
         backgroundImage: {
           'gradient-half': 'linear-gradient(to bottom, #FFFF 50%, #F0F0F0 50%)',
