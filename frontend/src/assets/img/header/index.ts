99137746ade02431fbe7afe79239b9155c6822cb
semnic <thenriqueleite277@gmail.com>
2025-03-13 20:50:27 -0300

    refactor

--- frontend/src/assets/img/header/index.ts
+++
@@ -1,8 +0,0 @@
-
-
-import { ReactComponent as IconCar } from "./car.svg";
-import { ReactComponent as IconPerfil } from "./perfil.svg";
-import { ReactComponent as IconSearch } from "./search.svg";
-import { ReactComponent as DropDawn } from "./dropdawn.svg";
-
-export {IconCar, IconPerfil, IconSearch, DropDawn}
